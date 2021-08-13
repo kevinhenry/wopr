24325eccd75dd46f7f6c8a1b22701ad8cdfc670c
Kevin Henry <kevinhenry@mac.com>
2021-08-12 20:34:37 -0700

    wopr youtube scraper working

--- wopr/wopr.py
+++ wopr/wopr.py
@@ -1,8 +1,8 @@
 from io import StringIO
 from pyfiglet import Figlet
-import re, click
-import requests
+import re, click, urllib, json, time, requests
 from requests.models import Response
+from selenium import webdriver
 
 
 f = Figlet(font="slant")
@@ -16,9 +16,9 @@ print(
   2. Copy the URL from that search.
   3. At the prompt, type the [store code], [search result URL for your item] and press [Enter]
 
-> [store code], [Search Result URL] <Enter>
+> <Coming Soon> [store code], [Search Result URL] <Enter>
 
-> Searching…!
+> <For Now> We took your keyword and applied that to the search on YouTube. Enjoy Searching…!
 """
 )
 
@@ -36,6 +36,9 @@ def main():
 def search(query):
     """This search query from YouTube Videos"""
     url_format = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q={}&type=video&key=AIzaSyAUuEmNYCYQcxsjztXWU14N5uqhW0lThD4"
+
+    # Search by ChannelID
+    # url_format = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=UCWr0mx597DnSGLFk1WfvSkQ&type=channelId&key=AIzaSyAUuEmNYCYQcxsjztXWU14N5uqhW0lThD4"
     # url_format = "https://googleapis.com/books/v1/volumes"
     query = "+".join(query.split())
 
@@ -49,15 +52,64 @@ def search(query):
 @main.command()
 @click.argument("id")
 def get(id):
-    api_key = "AIzaSyAUuEmNYCYQcxsjztXWU14N5uqhW0lThD4"
+    # api_key = "AIzaSyAUuEmNYCYQcxsjztXWU14N5uqhW0lThD4"
+    # channelId = "UCWr0mx597DnSGLFk1WfvSkQ"
     """This will return information about videos based upon your query"""
     url_format = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q={}&type=video&key=AIzaSyAUuEmNYCYQcxsjztXWU14N5uqhW0lThD4"
+    base_video_url = "https://www.youtube.com/watch?v="
+    # Search by ChannelID
+    # url_format = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=UCWr0mx597DnSGLFk1WfvSkQ&type=channelId&key=AIzaSyAUuEmNYCYQcxsjztXWU14N5uqhW0lThD4"
     # url_format = "https://www.googleapis.com/books/v1/volumes/{}"
     click.echo(id)
 
     response = requests.get(url_format.format(id))
-
     click.echo(response.json())
+
+    # with open('data.json', 'w') as f:
+    # json.dump(extracted_data, f, indent=4)
+
+
+# def parse(self, response):
+#     with open("data_file.json", "w") as filee:
+#         filee.write("[")
+#         for index, quote in enumerate(response.css("div.quote")):
+#             json.dump(
+#                 {
+#                     "text": quote.css("span.text::text").extract_first(),
+#                     "author": quote.css(".author::text").get(),
+#                     "tags": quote.css(".tag::text").getall(),
+#                 },
+#                 filee,
+#             )
+#             if index < len(response.css("div.quote")) - 1:
+#                 filee.write(",")
+#         filee.write("]")
+
+# json_data = requests.get(url_format).json()
+# json_status = json_data["status"]
+# print("API Status: " + json_status)
+
+
+#     inp = urllib.request.urlopen(url_format).read()
+#     resp = json.load(inp)
+#     vidID = resp["items"][0]["id"]["videoId"]
+
+#     video_exists = False
+#     with open("videoid.json", "r") as json_file:
+#         data = json.load(json_file)
+#         if data["videoID"] != vidID:
+#             driver = webdriver.FireFox()
+#             driver.get(base_video_url + vidID)
+#             video_exists = True
+
+#     if video_exists:
+#         with open("videoid.json", "w") as jason_file:
+#             data = {"videoId": vidID}
+#             json.dump(data, json_file)
+
+# try:
+#     while True"
+#     look
 
 
 if __name__ == "__main__":
